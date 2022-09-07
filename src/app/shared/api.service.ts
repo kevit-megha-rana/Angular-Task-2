bb4272d55620e11440bbf1c56806097f3205fece
kevit-megha-rana <megha.rana@kevit.io>
2022-09-07 12:56:07 +0530

    last commit

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -11,47 +11,19 @@ export class ApiService {
     constructor(private http:HttpClient){}
 
     getUser(){
-        return this.http.get<IUser[]>("http://localhost:3000/posts")
-        .pipe(
-            map(
-                (res:any) =>{
-                    return res;
-                }
-            )
-        );
+        return this.http.get<IUser[]>("http://localhost:3000/posts");
     }
 
     postUser(data:IUser){
-        return this.http.post<IUser>("http://localhost:3000/posts",data)
-        .pipe(
-            map(
-                (res:any) =>{
-                    return res;
-                }
-            )
-        );
+        return this.http.post<IUser>("http://localhost:3000/posts",data);
     }
 
     putUser(data:IUser,editId:number){
-        return this.http.put<IUser>("http://localhost:3000/posts/"+editId,data)
-        .pipe(
-            map(
-                (res:any) =>{
-                    return res;
-                }
-            )
-        );
+        return this.http.put<IUser>("http://localhost:3000/posts/"+editId,data);
     }
 
     deleteUser(editId:number){
-        return this.http.delete<IUser>("http://localhost:3000/posts/"+editId)
-        .pipe(
-            map(
-                (res:any) =>{
-                    return res;
-                }
-            )
-        );
+        return this.http.delete<IUser>("http://localhost:3000/posts/"+editId);
     }
 
 }
